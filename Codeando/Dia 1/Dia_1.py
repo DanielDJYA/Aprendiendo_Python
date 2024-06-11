d4e9487224a68c9163dee2d7826bac6e29283deb
DanielDJYA <yanez.31.12.98@gmail.com>
2024-06-11 11:33:31 -0500

    Phyton

---
+++ Codeando/Dia 1/Dia_1.py
@@ -0,0 +1,6 @@
+print("*************************")
+palabra_1 = input("hola como estas:")
+palabra_2 = 1
+combinar = palabra_1 + palabra_2
+print(f"La palabra combinada es {combinar}")
+print("*************************")
