64043269fede99f484c970ccf1e67df7bdf8f134
DanielDJYA <yanez.31.12.98@gmail.com>
2024-06-10 20:07:35 -0500

    como es

--- Codeando/dia 1/Dia_1.py
+++
@@ -1,6 +0,0 @@
-print("*************************")
-palabra_1 = input("hola como estas:")
-palabra_2 = 1
-combinar = palabra_1 + palabra_2
-print(f"La palabra combinada es {combinar}")
-print("*************************")
