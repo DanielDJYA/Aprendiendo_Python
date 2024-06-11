bae6f16ef636a25af1943f1adb1c25b495e29e60
DanielDJYA <yanez.31.12.98@gmail.com>
2024-06-11 11:29:39 -0500

    borrando

--- Codeando/Dia 2/Dia_2.py
+++
@@ -1,6 +0,0 @@
-print("*******************************************")
-ingreso = float(input("Ingresos del Usuario: "))
-porcentaje = 0.13
-calcular_porcentaje_ingreso = ingreso * porcentaje
-print(f"El resultado es: {round(calcular_porcentaje_ingreso,2)}")
-print("*******************************************")
